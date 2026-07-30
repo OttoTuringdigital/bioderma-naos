43f223034874d162cc2f9ff7ea4723bfe421027f
Otto <92496712+OttoTuringdigital@users.noreply.github.com>
2026-07-30 13:40:46 +0800

    p v1.0.8

--- dist/td-figma.product.v1.0.8.js
+++ dist/td-figma.product.v1.0.8.js
@@ -1,8 +1,8 @@
-/* TD_Figma_Product_Page_GTM_v1.0.7.html */
+/* TD_Figma_Product_Page_GTM_v1.0.8.html */
 (function () {
   'use strict';
 
-  var VERSION = '1.0.7';
+  var VERSION = '1.0.8';
   var INIT_RETRY_LIMIT = 240;
   var SOURCE_RETRY_LIMIT = 100;
   var SOURCE_STABLE_REQUIRED = 4;
